bf412df4512ee2a72e6df29dd3064982ffc3af7d
minhhuy00600 <minhhuy00600@gmail.com>
2021-12-06 18:14:23 +0700

    Sửa thêm 1 chút cho đẹp

--- Client/Header_client.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<WinSock2.h>
-#include<string>
-
-#pragma comment (lib, "Ws2_32.lib")
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define DEFAULT_PORT "27015"
-
-using namespace std;
